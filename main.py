cde8323524a1af387d42294a2f48717faacd7354
ajamol2197 <ajamol2197@gmail.com>
2026-02-18 16:52:31 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+# 1
+numbers = (15, 8, 22, 3, 19)
+print(max(numbers))
+
+# 2
+numbers = (4, 9, 16, 25, 36)
+print(numbers)
+
+if 16 in numbers:
+    print("bor")
+else:
+    print("yoq")
+
+# 3
+numbers = (1, 2, 3, 4, 5)
+print(numbers)
+
+numbers = list(numbers)
+numbers.sort(reverse=True)
+print(numbers)
+
+# 4
+numbers = (12, 7, 18, 5, 20)
+print(numbers)
+
+if numbers == 2:
+    print(" bor")
+else:
+    print("yoq")
+
+# 5
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+
+t3 = t1 + t2
+print(t3)
+
+6
+data = ("Ali", 25, "Toshkent", "Dasturchi")
+print(data[2])
+
+# 7
+words = ("python", "code", "AI", "developer")
+print(words)
+
+for word in words:
+    print(len(word))
+
+# 8
+numbers = (7, 8)
+print(numbers)
+
+for a in range(3):
+    print(numbers)
+
+9
+numbers = (45, 12, 78, 5, 34)
+print(min(numbers))
+
+# 10
+numbers = (3, 6, 9, 12)
+print(sum(numbers))
